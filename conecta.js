fefac5b45c30672227dfa0d38a0c3678ea8d5e9a
Lucas Viana <lucasviana112@gmail.com>
2019-07-18 20:15:25 -0300

    att

--- conecta.js
+++ conecta.js
@@ -1,11 +1,18 @@
 const mysql = require("mysql");
 
 //pool de conexoes ao banco gerenciados pelo mysql
-let conexao = mysql.createPool({
+/*let conexao = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'focus'
+});*/
+
+let conexao = mysql.createPool({
+    host: 'sql10.freesqldatabase.com',
+    user: 'sql10299044',
+    password: '2Hq2eb4mkH',
+    database: 'sql10299044'
 });
 
 
--- models/db.js
+++ models/db.js
@@ -1,7 +1,13 @@
 const Sequelize = require("sequelize")
 
-const sequelize = new Sequelize('focus', 'root', '',{
+/*const sequelize = new Sequelize('focus', 'root', '',{
     host: 'localhost',
+    dialect: 'mysql',
+    timezone: 'America/Sao_Paulo'
+})*/
+
+const sequelize = new Sequelize('sql10299044', 'sql10299044', '2Hq2eb4mkH',{
+    host: 'sql10.freesqldatabase.com',
     dialect: 'mysql',
     timezone: 'America/Sao_Paulo'
 })
