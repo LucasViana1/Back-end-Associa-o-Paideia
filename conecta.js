098023f64dfef5b23a2964791e0465c82104f972
Lucas Viana <lucasviana112@gmail.com>
2019-07-31 19:20:09 -0300

    att cred

--- conecta.js
+++ conecta.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 
 //pool de conexoes ao banco gerenciados pelo mysql
-/*let conexao = mysql.createPool({
+let conexao = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'focus'
-});*/
-
+});
+/*
 let conexao = mysql.createPool({
     host: '35.184.237.68',
     user: 'root',
     password: 'B9BkPJp6xd3z',
     database: 'focus'
-});
+});*/
 
 
 //exportar a conexao para outros modulos
