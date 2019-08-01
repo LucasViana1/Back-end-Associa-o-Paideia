67c0eefdc6154e1132a4aa68e4231449cabb2b2a
Lucas Viana <lucasviana112@gmail.com>
2019-08-01 10:06:11 -0300

    teste var de ambiente

--- email/ServicoEmail.js
+++ email/ServicoEmail.js
@@ -6,8 +6,8 @@ var transporter = nodemailer.createTransport({
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-        user: 'EMAIL',
-        pass: 'SENHA'
+        user: 'devopsprog@gmail.com',
+        pass: '98111111'
     },
     tls: {
         rejectUnauthorized: false//rejeita autorização acesso ao email, talvez remover
--- models/db.js
+++ models/db.js
@@ -1,11 +1,16 @@
 const Sequelize = require("sequelize")
 
-const sequelize = new Sequelize('focus', 'root', '',{
+/*const sequelize = new Sequelize('focus', 'root', '',{
     host: 'localhost',
     dialect: 'mysql',
     timezone: 'America/Sao_Paulo'
-})
+})*/
 
+const sequelize = new Sequelize('focus', 'root', 'B9BkPJp6xd3z',{
+    host: '35.184.237.68',
+    dialect: 'mysql',
+    timezone: 'America/Sao_Paulo'
+})
 
 module.exports = {
     Sequelize: Sequelize,
