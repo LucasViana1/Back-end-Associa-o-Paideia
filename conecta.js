e786d7f012e4fb38ac3b7339475647180c3dd825
Lucas Viana <lucasviana112@gmail.com>
2019-08-02 09:59:26 -0300

    configurando uso de variaveis internas em ambiente de producao e desenvolviment (auto-adaptacao)

--- conecta.js
+++ conecta.js
@@ -9,11 +9,14 @@ const mysql = require("mysql");
 });*/
 
 let conexao = mysql.createPool({
-    host: '35.184.237.68',
-    //user: process.env.DBUSER,
+    /*host: '35.184.237.68',
     user: 'root',
     password: 'B9BkPJp6xd3z',
-    database: 'focus'
+    database: 'focus'*/
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBDATABASE
 });
 
 
--- models/db.js
+++ models/db.js
@@ -6,8 +6,14 @@ const Sequelize = require("sequelize")
     timezone: 'America/Sao_Paulo'
 })*/
 
-const sequelize = new Sequelize('focus', 'root', 'B9BkPJp6xd3z',{
+/*const sequelize = new Sequelize('focus', 'root', 'B9BkPJp6xd3z',{
     host: '35.184.237.68',
+    dialect: 'mysql',
+    timezone: 'America/Sao_Paulo'
+})*/
+
+const sequelize = new Sequelize(process.env.DBDATABASE, process.env.DBUSER, process.env.DBPASSWORD,{
+    host: process.env.DBHOST,
     dialect: 'mysql',
     timezone: 'America/Sao_Paulo'
 })
